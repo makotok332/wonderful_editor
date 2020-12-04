b2a46b76cf8adc2063086a89b9bf34491534cb6e
木村誠 <>
2020-12-05 00:11:54 +0900

    RSpec/MultipleExpectations: Example has too many expectations回避のため ragistration_request_spec に aggregate_failures "最後まで通過" do end を追記

--- spec/requests/api/v1/auth/registrations_request_spec.rb
+++ spec/requests/api/v1/auth/registrations_request_spec.rb
@@ -6,42 +6,54 @@ RSpec.describe "Api::V1::Auth::Registrations", type: :request do
 
     context "適切なパラメーターを送信したとき" do
       let(:params) { { user: attributes_for(:user) } }
-      it "ユーザーを新規登録できる" do
-        expect { subject }.to change { User.count }.by(1)
-        res = JSON.parse(response.body)
-        expect(res["data"]["name"]).to eq params[:user][:name]
-        expect(res["data"]["email"]).to eq params[:user][:email]
-        expect(response).to have_http_status(:ok)
+
+      it "ユーザーの新規登録できる" do
+        aggregate_failures "最後まで通過" do
+          expect { subject }.to change { User.count }.by(1)
+          expect(response).to have_http_status(:ok)
+          res = JSON.parse(response.body)
+          expect(res["data"]["name"]).to eq params[:user][:name]
+          expect(res["data"]["email"]).to eq params[:user][:email]
+        end
       end
     end
 
     context "nameがないとき" do
       let(:params) { { user: attributes_for(:user, name: nil) } }
+
       it "エラーする" do
-        expect { subject }.to change { User.count }.by(0)
-        res = JSON.parse(response.body)
-        expect(response).to have_http_status(:unprocessable_entity)
-        expect(res["errors"]["name"]).to eq ["can't be blank"]
+        aggregate_failures "最後まで通過" do
+          expect { subject }.to change { User.count }.by(0)
+          res = JSON.parse(response.body)
+          expect(response).to have_http_status(:unprocessable_entity)
+          expect(res["errors"]["name"]).to eq ["can't be blank"]
+        end
       end
     end
 
     context "emailがないとき" do
       let(:params) { { user: attributes_for(:user, email: nil) } }
+
       it "エラーする" do
-        expect { subject }.to change { User.count }.by(0)
-        res = JSON.parse(response.body)
-        expect(response).to have_http_status(:unprocessable_entity)
-        expect(res["errors"]["email"]).to eq ["can't be blank"]
+        aggregate_failures "最後まで通過" do
+          expect { subject }.to change { User.count }.by(0)
+          res = JSON.parse(response.body)
+          expect(response).to have_http_status(:unprocessable_entity)
+          expect(res["errors"]["email"]).to eq ["can't be blank"]
+        end
       end
     end
 
     context "passwordがないとき" do
       let(:params) { { user: attributes_for(:user, password: nil) } }
+
       it "エラーする" do
-        expect { subject }.to change { User.count }.by(0)
-        res = JSON.parse(response.body)
-        expect(response).to have_http_status(:unprocessable_entity)
-        expect(res["errors"]["password"]).to eq ["can't be blank"]
+        aggregate_failures "最後まで通過" do
+          expect { subject }.to change { User.count }.by(0)
+          res = JSON.parse(response.body)
+          expect(response).to have_http_status(:unprocessable_entity)
+          expect(res["errors"]["password"]).to eq ["can't be blank"]
+        end
       end
     end
   end
