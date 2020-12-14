7dbfb5e79e915890b3a304281b70779cbfc4b12c
木村誠 <>
2020-12-14 21:18:51 +0900

    ダミーコード current_userの削除 current_user, authenticate_user!, user_signed_in? の定義 article_rquest_specの修正

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,12 +1,7 @@
-# class ArticlesController < ApplicationController
-#   def index
-#     articles = Article.all
-#     render json: articles
-#   end
-# end
-
 module Api::V1
   class ArticlesController < BaseApiController
+    before_action :authenticate_user!
+
     def index
       articles = Article.order(updated_at: :desc)
       render json: articles, each_serializer: Api::V1::ArticlePreviewSerializer
