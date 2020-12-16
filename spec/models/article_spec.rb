88cf7f533ecc2e12e5de9cd6c8a3b84b3f6b5401
木村誠 <>
2020-12-17 01:12:40 +0900

    articlesテーブルにstatusカラムを追加 enum でstatusにdraftとpublishedを定義 下書き、公開状態での記事作成のテストの実装

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -4,6 +4,7 @@
 #
 #  id         :bigint           not null, primary key
 #  body       :text
+#  status     :string
 #  title      :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
@@ -31,6 +32,22 @@ RSpec.describe Article, type: :model do
     let(:article){build(:article, title: nil)}
     it "記事の作成に失敗" do
       expect(article).not_to be_valid
+    end
+  end
+
+  context "statusがdraftのとき"do
+    let(:article){build(:article, :draft)}
+    fit "記事が下書きで作成される" do
+      expect(article).to be_valid
+      expect(article.status).to eq "draft"
+    end
+  end
+
+  context "statusがpublishedのとき"do
+  let(:article){build(:article, :published)}
+    it "記事が公開で作成される" do
+      expect(article).to be_valid
+      expect(article.status).to eq "published"
     end
   end
 
