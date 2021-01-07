5890936c55b75edfd84df085d392016d0f2cb4fd
木村誠 <>
2021-01-07 21:50:41 +0900

    no message

--- app/serializers/api/v1/article_serializer.rb
+++ app/serializers/api/v1/article_serializer.rb
@@ -1,4 +1,4 @@
-class ArticleSerializer < ActiveModel::Serializer
+class Api::V1::ArticleSerializer < ActiveModel::Serializer
   attributes :id, :title, :body, :updated_at, :status
   belongs_to :user, serializer: Api::V1::UserSerializer
 end
